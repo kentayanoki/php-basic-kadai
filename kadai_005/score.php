5ce696421033ab9ab4135b66507a56c4c6b6533b
kentayanoki <yanoki111122@gmail.com>
2024-02-16 10:48:14 +0900

    課題5を実施

---
+++ kadai_005/score.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_005</title>
+</head>
+
+<body>
+    
+    <?php
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+    // スコアの合計点定義
+    $score_sum = $score1 + $score2+ $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+    // スコアの平均点定義
+    $score_ave = $score_sum / 10;
+    echo $score_sum;
+    echo '<br>';
+    echo $score_ave;
+    ?>
+</body>
+
+</html>
